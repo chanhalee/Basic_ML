867c1d82d2d7f7f7edc2ef0ef88115d5cbfa571b
LeeCH <chan771788@gmail.com>
2021-08-15 00:27:37 +0900

    202108140020 Node의 데이터 저장소 역할을 단순한 백업도구로 축소 IdentityData클래스 삭제, serial을 통해 각 노드 구분.
    202108140028
    edge weightadjust를 실행하는 노드의 메서드 구현필요
    
    202108141906
    흥분 매커니즘- 방파제모델 구상.
    
    202108142222
    edge를 파일로부터 읽어오는 메서드 구현 완료.
    
    202108150027
    읽어들어온 정보를 조합하여 하나의 HashMap 으로 갖는 메서드 구현완료.

--- src/Entity.java
+++ src/Entity.java
@@ -29,7 +29,7 @@ import java.util.logging.Level;
 /*-------------------------------1.개체의 기본형-------------------------------*/
 
 public class Entity {
-    private HashMap<LevelData, HashMap> mindCircuit = new HashMap<>(); // (levelData -(Node - Edge))의 구성.
+    private HashMap<LevelData, HashMap<Node, HashSet<Edge>>> mindCircuit; // (levelData -(Node - Edge))의 구성.
     private HashSet<Node> previousSparkedNode = new HashSet<>();
     private HashSet<Node> currentSparkedNode = new HashSet<>();
     ArrayList<String> inputFileList;
@@ -38,7 +38,9 @@ public class Entity {
     Entity(ArrayList<String> inputFileList, ArrayList<String> outputFileList){
         this.inputFileList = inputFileList;
         this.outputFileList = outputFileList;
-
+        mindCircuit =  Matcher(readNodeFromFile(inputFileList.get(0)), readEdgeFromFile(inputFileList.get(1)));
+        HashMap<Integer, HashSet<Edge>> tempEdgeMap = readEdgeFromFile(inputFileList.get(1));
+        /*HashMap<LevelData, HashMap> */
     }
     //Entity(){};
 
@@ -46,15 +48,12 @@ public class Entity {
 
 
 
-    private HashMap readNodeFromFile(String inputFile){// Node 만 읽어 ArrayList 로 저장, level 과 묶어 map 을 만들어 반환.
-        HashMap<LevelData, HashSet> result = new HashMap<>();
+    private HashMap<LevelData, HashSet<Node>> readNodeFromFile(String inputFile){// Node 만 읽어 ArrayList 로 저장, level 과 묶어 map 을 만들어 반환.
+        HashMap<LevelData, HashSet<Node>> result = new HashMap<>();
         LevelData levelData = null;
         HashSet<Node> nodeData = null;
         String fileLine = "";
         ArrayList<String> splitInputData = null;
-
-
-
         try {
             FileReader fileInput = new FileReader(inputFile);
             BufferedReader inputBuffer = new BufferedReader(fileInput);
@@ -72,9 +71,9 @@ public class Entity {
                 }
                 fileLine = fileLine.replace("[", "");
                 splitInputData = new ArrayList<String>(Arrays.stream(fileLine.split("]")).toList());
+                assert nodeData != null;        // 컴파일러가 처리한 코드. assert 명령어 공부 필요.
                 nodeData.add(makeNodeFromAList(splitInputData));
             }
-
         } catch (IOException ie){
             ie.printStackTrace();
         }
@@ -90,8 +89,6 @@ public class Entity {
     private Node makeNodeFromAList(ArrayList<String> data){
         Node node = null;
         int nodeFormat = Integer.parseInt(data.get(0).trim());
-
-
         try {
             node = switch (nodeFormat) {
                 case InputNodeInter.NODE_FORMAT -> new InputNode(data.get(1).trim(), Integer.parseInt(data.get(2).trim()), Double.parseDouble(data.get(3).trim()), Boolean.parseBoolean(data.get(4).trim()));
@@ -103,40 +100,39 @@ public class Entity {
             System.out.println("에러 메시지: " + ne.getMessage());
             ne.printStackTrace();
         }
-
         return node;
     }
 
-    private HashMap<Integer, Edge> readEdgeFromFile(String inputFile){
-        HashMap<Integer, Edge> result = new HashMap<>();
+
+    private HashMap<Integer, HashSet<Edge>> readEdgeFromFile(String inputFile){  // 파일로부터 엣지 정보를 읽어 시작노드 시리얼과 엣지를 묶어 저장.
+        HashMap<Integer, HashSet<Edge>> result = new HashMap<>();
         String fileLine = "";
         ArrayList<String> splitInputData = null;
-
         try {
             FileReader fileInput = new FileReader(inputFile);
             BufferedReader inputBuffer = new BufferedReader(fileInput);
-
             for (int i = 1; (fileLine = inputBuffer.readLine()) != null; i++){  /// 파일에서 라인 읽어오기
                 if(fileLine.trim().startsWith("$$"))    //라인의 가장 앞에 나오는 $$는 주석역할
                     continue;
                 fileLine = fileLine.replace("[", "");
                 splitInputData = new ArrayList<String>(Arrays.stream(fileLine.split("]")).toList());
-                result.put(Integer.valueOf(splitInputData.get(1)), makeEdgeFromAList(splitInputData));
+                if(!result.containsKey(Integer.valueOf(splitInputData.get(1)))){
+                    HashSet<Edge> tempHashSet = new HashSet<Edge>();
+                    result.put(Integer.valueOf(splitInputData.get(1)), tempHashSet);
+                }
+                result.get(Integer.valueOf(splitInputData.get(1))).add(makeEdgeFromAList(splitInputData));
             }
         }catch (IOException ie){
             ie.printStackTrace();
         }
-
         return result;
     }
-
 
 
     private Edge makeEdgeFromAList(ArrayList<String> data){
         Edge edge = null;
         Integer startNodeSerial;
         int edgeFormat = Integer.parseInt(data.get(0).trim());
-
         try {
             if(edgeFormat == 1) {
                 if (data.size() == 5)
@@ -145,24 +141,53 @@ public class Entity {
                     edge = new Edge(Integer.parseInt(data.get(1)), Integer.parseInt(data.get(2)), Double.parseDouble(data.get(3)));
                 else
                     throw new InValidEdgeFormatException("정의되지 않은 Edge 생성자 매개변수 개수 값: " + data.size());
-
             }else if(edgeFormat == 2){
-
                 if(data.size() == 6)
                     edge = new LoopEdge(Integer.parseInt(data.get(1)), Integer.parseInt(data.get(2)), Double.parseDouble(data.get(3)), Double.parseDouble(data.get(4)), Integer.parseInt(data.get(5)));
                 else if(data.size() == 5)
                     edge = new LoopEdge(Integer.parseInt(data.get(1)), Integer.parseInt(data.get(2)), Double.parseDouble(data.get(3)), Integer.parseInt(data.get(4)));
                 else throw new InValidEdgeFormatException("정의되지 않은 LoopEdge 생성자 매개변수 개수 값: " + data.size());
-
             }
             else throw new InValidEdgeFormatException("정의되지 않은 EDGE_FORMAT 입력. 값: " + edgeFormat);
-
         } catch(InValidEdgeFormatException ee){
             System.out.println("에러 메시지: " + ee.getMessage());
             ee.printStackTrace();
         }
-
         return edge;
+    }
+
+
+    private HashMap<LevelData, HashMap<Node, HashSet<Edge>>> Matcher(HashMap<LevelData, HashSet<Node>> nodeMap, HashMap<Integer, HashSet<Edge>> edgeMap){
+        HashMap<LevelData, HashMap<Node, HashSet<Edge>>> result = new HashMap<>();
+        HashMap<Node, HashSet<Edge>> NodeNEdgeMap = new HashMap<>();
+        HashSet<Node> tempNodeSet = new HashSet<>();
+        for(LevelData ld: nodeMap.keySet()){
+            result.put(ld, new HashMap<Node, HashSet<Edge>>());
+        }
+        for(HashSet<Node> i :nodeMap.values()) {
+            tempNodeSet.addAll(i);
+        }
+        for(Integer i : edgeMap.keySet()){
+            for(Node n : tempNodeSet){
+                if(n.SERIAL_NUMBER == i){
+                    NodeNEdgeMap.put(n, edgeMap.get(i));
+                    edgeMap.remove(i);
+                    tempNodeSet.remove(n);
+                }
+            }
+        }
+        for(LevelData ld: nodeMap.keySet()){
+            HashSet<Node> ns = nodeMap.get(ld);
+            for(Node n : NodeNEdgeMap.keySet()){
+                if(ns.contains(n)){
+                    result.get(ld).put(n, NodeNEdgeMap.get(n));
+                }
+            }
+        }
+
+
+
+        return result;
     }
 
 
@@ -247,8 +272,7 @@ class LevelData{
 
     @Override
     public int hashCode() {
-        int result = (int) (NODE_FORMAT * 1000 + LEVEL);
-        return result;
+        return (int) (NODE_FORMAT * 1000 + LEVEL);
     }
 
     @Override
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
-
+        HashMap<Integer, HashMap> test1= new HashMap<>();
+        HashMap<Integer, Integer> test = new HashMap<>();
+        test.put(1, null);
+        System.out.println(test);
+        test.put(1, 2);
+        System.out.println(test);
+        test.put(1,3);
+        System.out.println(test);
+        test1.put(1, test);
+        test.put(1,1);
+        test1.put(2, test);
+        System.out.println(test1);
+        test.put(2, 2);
+        System.out.println(test1);
     }
 }
